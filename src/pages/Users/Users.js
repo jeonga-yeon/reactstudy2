85ebb5d07e27c5da088f622fb7325231946318e0
joyfuldev <wings2132@naver.com>
2023-05-05 23:32:18 +0900

    Add: 쿼리 라우팅 완료

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,26 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import CardList from "./CardList/CardList";
 import "./Users.scss";
 
 function Users() {
   const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  const [queryString, setQueryString] = useSearchParams();
+
+  const start = queryString.get("start");
+  const limit = queryString.get("limit");
+
+  const handleQuery = (event) => {
+    const offset = (Number(event.target.innerHTML) - 1) * 20;
+    const LIMIT = 20;
+    navigate(`?start=${offset}&limit=${LIMIT}`);
+  };
 
   useEffect(() => {
-    fetch("http://localhost:8000/users")
+    fetch(`http://localhost:8000/users?start=${start}&limit=${limit}`)
       .then((res) => res.json())
       .then((res) => setUsers(res.users));
-  }, []);
+  }, [start, limit]);
 
   return (
     <div className="users">
       <h1>Assignment - Query String</h1>
       <div className="pageBtn">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+        <button onClick={handleQuery}>1</button>
+        <button onClick={handleQuery}>2</button>
+        <button onClick={handleQuery}>3</button>
+        <button onClick={handleQuery}>4</button>
+        <button onClick={handleQuery}>5</button>
       </div>
       <CardList users={users} />
     </div>
